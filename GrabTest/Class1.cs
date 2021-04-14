61d15767137c23df8dfa73764edb9af50ca4cb8b
Ahmad Syafiq <asyafiq99@gmail.com>
2021-04-15 00:16:56 +0800

    Initial

---
+++ GrabTest/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+	public class Class1
+	{
+		public int solution(int[] A)
+		{
+			int days = 30;
+			int oneDayTicket = 2;
+			int sevenDayTicket = 7;
+			int thirtyDayTicket = 25;
+
+			int length = A.Length;
+
+			int cost = 0;
+			if (length == days)
+			{
+				cost = thirtyDayTicket;
+			}
+			else
+			{
+				int minDate = A[0];
+				int maxDate = A[A.Length - 1];
+
+
+				for (int i = 0; i < A.Length; i++)
+				{
+					if (cost >= 25)
+					{
+						cost = thirtyDayTicket;
+						break;
+					}
+
+					if (minDate > maxDate)
+						break;
+
+					int next7Days = minDate + 6;
+
+					int nearest = A.OrderBy(x => Math.Abs((long)x - next7Days)).First();
+
+					int nearIndex = Array.FindIndex(A, x => x == nearest);
+					int nextIndex = nearIndex + 1;
+					int nextDate = 31;
+
+					if (nextIndex < A.Length)
+					{
+						nextDate = A[nextIndex];
+						i = nearIndex;
+					}
+
+					int dayDiff = nearest - minDate;
+
+					if (dayDiff >= 3)
+					{
+						cost += sevenDayTicket;
+					}
+					else
+					{
+						cost += oneDayTicket;
+					}
+					minDate = nextDate;
+				}
+
+			}
+			return cost;
+		}
+	}
+
+}
---
+++ GrabTest/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Question 1 get fee for parking
+
+			string E = "10:00";
+			string L = "12:30";
+
+			TimeSpan startTime = TimeSpan.Parse(E);
+			TimeSpan endTime = TimeSpan.Parse(L);
+
+			int entranceFee = 2;
+			int firstHour = 3;
+			int subSequence = 4;
+
+			TimeSpan hours = endTime - startTime;
+
+			int fee = 0;
+			if (hours > new TimeSpan(1, 0, 0))
+			{
+				int subHours = (hours - new TimeSpan(1, 0, 0)).Hours;
+				if (hours.Minutes > 0)
+				{
+					++subHours;
+				}
+				fee = entranceFee + firstHour + (subHours * subSequence);
+			}
+			else
+			{
+				fee = entranceFee + firstHour;
+			}
+
+
+			//Question 2 get cheapest ticket price
+			Class1 class1 = new Class1();
+
+			//int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+			int[] array = { 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+			//int[] array = { 1, 2, 3, 4, 5, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+
+
+			Console.WriteLine(class1.solution(array));
+			Console.ReadLine();
+
+
+
+
+		}
+	}
+}
